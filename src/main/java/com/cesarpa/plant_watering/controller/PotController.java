7d038bb4e77a281132135f2b7616adf7a5eaf186
Cesarpa Code <cesar.1023@hotmail.com>
2026-03-29 13:26:23 -0500

    feat: updating .MD

--- src/main/java/com/cesarpa/plant_watering/controller/PotController.java
+++ src/main/java/com/cesarpa/plant_watering/controller/PotController.java
@@ -3,11 +3,13 @@ package com.cesarpa.plant_watering.controller;
 import com.cesarpa.plant_watering.model.Pot;
 import com.cesarpa.plant_watering.service.ServicePotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/pot")
 public class PotController {
 
@@ -15,13 +17,15 @@ public class PotController {
     private ServicePotService servicePotService;
 
     @GetMapping
-    public List<Pot> getAll() {
+    public List<Pot> listPots(Model model) {
         return servicePotService.getAll();
+
     }
 
     @PostMapping
     public Pot save(@RequestBody Pot pot) {
-        return servicePotService.save(pot);
+        servicePotService.save(pot.getId(), pot);
+        return ""
     }
 
     @DeleteMapping("/{id}")
